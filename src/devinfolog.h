2e4b1c17e38b36a3540596b41286f30da1d4eff8
Yuriy <79077842+Sinfolke@users.noreply.github.com>
2024-07-26 19:02:50 +0300

    Do not add platform log when just testing

--- src/devinfolog.h
+++ src/devinfolog.h
@@ -1,5 +1,5 @@
 void devinfolog(void) {
-    # ifdef DEBUG
+    # if defined(DEBUG) && !defined(NO_HEAD)
         std::cout << "\t#########################" << '\n';
             std::cout << "\t#\tDEBUG BUILD\t#\t" << '\n';
             std::cout << "\t#\t" << devinfo::ARCH_STR << " x" << devinfo::ARCH_BITSIZE << "\t#\t" << '\n';
