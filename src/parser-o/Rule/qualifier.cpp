b7ee4a4627cc2bfd6eef80bd7fb22274129a6f72
Sinfolke <uravasinski@gmail.com>
2024-11-13 08:45:12 +0200

    Progress of converting parser rules to C++ [12]

--- src/parser-o/Rule/qualifier.cpp
+++ src/parser-o/Rule/qualifier.cpp
@@ -1,2 +1,9 @@
 #include <parser.h>
 #include <parser_defs.h>
+
+Rule(Rule_qualifier) {
+    auto pos = in;
+    if (*pos != '?' && *pos != '+' && *pos != '*')
+        return {};
+    RULE_SUCCESSD(in, pos, Rule_qualifier, *pos);
+}
