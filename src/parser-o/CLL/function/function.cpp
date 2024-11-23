62dd0f84d20e62d28dc805b867e87233f0268010
Sinfolke <uravasinski@gmail.com>
2024-11-23 09:11:45 +0200

    mostly finished convertion [19]

--- src/parser-o/CLL/function/function.cpp
+++ src/parser-o/CLL/function/function.cpp
@@ -1,6 +1,8 @@
 // not gonna try declare these helper functions instead copy-paste their content as more quick solution
 // static void function_body(void);
 // static void function_in_parentheses(void);
+#include <parser/parser.h>
+#include <parser/parser_defs.h>
 
 Rule(function_body_call) {
     auto pos = in;
@@ -29,4 +31,64 @@ Rule(function_body_decl) {
         return {};
     
     RULE_SUCCESSD(in, pos, function_body_call, function_parameters_res.token);
+}
+Rule(function_arguments) {
+    auto pos = in;
+    auto any_data_res = any_data(pos);
+    std::vector<Rule> _3 {};
+    if (any_data_res.result) {
+        pos += any_data_res.token.length();
+        while (*pos == ',') {
+            pos++;
+            auto any_data2_res = any_data(pos);
+            if (!any_data2_res)
+                break;
+            pos += any_data2_res.token.length();
+            _3.push_back(any_data2_res.token);
+        }
+    }
+    
+    std::vector<Rule> data {
+        any_data_res.token,
+        _3
+    };
+    RULE_SUCCESSD(in, pos, function_arguments, data);
+}
+Rule(function_parameters) {
+    auto pos = in;
+    auto id_res = id(pos);
+    std::vector<Rule> _3 {};
+    if (id_res.result) {
+        pos += id_res.token.length();
+        while (*pos == ',') {
+            pos++;
+            auto id2_res = id(pos);
+            if (!id2_res)
+                break;
+            pos += id2_res.token.length();
+            _3.push_back(id2_res.token);
+        }
+    }
+    
+    std::vector<Rule> data {
+        id_res.token,
+        _3
+    };
+    RULE_SUCCESSD(in, pos, function_arguments, data);
+}
+Rule(cll_function_call) {
+    auto pos = in;
+    auto id_res = id(pos);
+    if (!id_res.result)
+        return {};
+    pos += id_res.token.length();
+    auto function_body_call_res = function_body_call(pos);
+    if (!function_body_call_res.result)
+        return {};
+    
+    std::unordered_map<const char*, std::any> data {
+        { "name", id_res.token },
+        { "body", function_body_call_res.token }
+    };
+    RULE_SUCCESSD(in, pos, cll_function_call, data);
 }
