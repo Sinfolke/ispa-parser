119b69eae589a2a2adc5a4d8a362c733ec40f173
Sinfolke <uravasinski@gmail.com>
2024-12-16 20:09:07 +0200

    Added for loop

--- src/CLL/loops.cpp
+++ src/CLL/loops.cpp
@@ -26,4 +26,44 @@ Rule(loop_while) {
 }
 Rule(loop_for) {
     auto pos = in;
+    if (strncmp(pos, "for", 3))
+        return {};
+    pos += 3;
+    if (*pos != '(')
+        return {};
+    pos++;
+    auto expr1_res = expr(pos);
+    if (expr1_res.result)
+        pos += expr1_res.token.length();
+    if (*pos != ';')
+        return {};
+    auto expr2_res = expr(pos);
+    if (expr2_res.result)
+        pos += expr2_res.token.length();
+    
+    if (*pos != ';')
+        return {};
+    
+    auto expr3_res = expr(pos);
+    if (expr3_res.result)
+        pos += expr3_res.token.length();
+
+    if (*pos != ')')
+        return {};
+    pos++;
+
+    auto block = cll_block(pos);
+    if (!block.result) {
+        // skip spaces here
+        block = cll_spaced_block(pos, );
+        if (!block.result)
+            return {};
+    }
+
+    std::unordered_map<const char*, std::any> data {
+        { "decl", expr1_res.token },
+        { "cond", expr2_res.token },
+        { "end", expr3_res.token },
+        { "block", block.token }
+    };
 }
