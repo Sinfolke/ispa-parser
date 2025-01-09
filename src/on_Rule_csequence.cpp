5ae4d11d5519f0edff38ecbe92cb8831d8b9c78f
Sinfolke <uravasinski@gmail.com>
2025-01-09 19:51:25 +0200

    Added on_string

--- src/on_Rule_csequence.cpp
+++ src/on_Rule_csequence.cpp
@@ -1,20 +1,28 @@
 #include <defs.h>
 #include <debug/logging.h>
+bool csequence_not;
 void on_Rule_csequence(Parser::Tree &tree, int &i, use_prop_t &use_prop, std::string &buf, Parser::Rule member) {
     if (!processingToken)
         throw Error("processing Rule_csequence in non-token rule");
+    auto data = std::any_cast<obj_t>(member.data);
+    csequence_not = std::any_cast<bool>(corelib::map::get(data, "not"));
     if (qualifier == '+' || qualifier == '*') {
-        buf += "while (!(true";
+        buf += "while";
+        if (!csequence_not)
+            buf += "!(";
+        buf += "true";
     } else {
-        buf += "if (!(true";
+        buf += "if";
+        if (!csequence_not)
+            buf += "!(";
+        buf += "true";
     }
 }
 void on_Rule_csequence_close(Parser::Tree &tree, int &i, use_prop_t &use_prop, std::string &buf, Parser::Rule member) {
-    buf += R"())
-    {
-        return {};
-    }
-    )";
+    buf += ')';
+    if (!csequence_not)
+        buf += ')';
+    buf += "\n\treturn {};";
 }
 void on_Rule_csequence_diapason(Parser::Tree &tree, int &i, use_prop_t &use_prop, std::string &buf, Parser::Rule member) {
     auto data = std::any_cast<arr_t<Parser::Rule>>(member.data);
