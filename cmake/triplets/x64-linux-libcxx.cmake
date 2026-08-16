bd13d6d0872002f4b2b939e8d885d0c780103b16
Sinfolke <uravasinski@gmail.com>
2026-08-16 08:58:07 +0300

    Fix some github actions compile time errors

--- cmake/triplets/x64-linux-libcxx.cmake
+++ cmake/triplets/x64-linux-libcxx.cmake
@@ -3,7 +3,7 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
 set(VCPKG_CXX_FLAGS "-stdlib=libc++")
-set(VCPKG_C_FLAGS "-stdlib=libc++")
+set(VCPKG_C_FLAGS "")
 
 set(VCPKG_LINKER_FLAGS "-stdlib=libc++")
 
